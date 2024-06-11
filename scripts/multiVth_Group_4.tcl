38a592ad3d8bb791a13e98ae34a41135bef1bd6c
Lorenzo Badas <lorenzo.badas@gmail.com>
2024-06-11 17:48:14 +0200

    add scheleton for binary_swap function

--- scripts/multiVth_Group_4.tcl
+++ scripts/multiVth_Group_4.tcl
@@ -1,78 +1,65 @@
-proc get_cells_in_path {path} {
-    set timing_points [get_attribute $path points]
-    set result_cells_list [list]
-    set cells_name_list [list]
-    foreach_in_collection timing_point $timing_points {
-        set timing_point_object [get_attribute $timing_point object]
-        set timing_point_object_class [get_attribute $timing_point_object object_class]
-        # if timing_point_object_class is pin then get the cell
-        if {$timing_point_object_class eq "pin"} {
-            set pin [get_attribute $timing_point_object full_name]
-            set cells_list [get_cells -of_objects $pin]
-            foreach_in_collection cell $cells_list {
-                set cell_full_name [get_attribute $cell full_name]
-                # if cell is not in the list add it
-                if {[lsearch -exact $cells_name_list $cell_full_name] == -1} {
-                    lappend cells_name_list $cell_full_name
-                    lappend result_cells_list $cell
-                }
-            }
-        }
-    }
-    return $result_cells_list
-}
-
 proc create_priority_list {} {
-    # count number of cells
     set all_cells [get_cells]
-    set cell_count [sizeof_collection $all_cells]
     set priority_list [list]
-    # get $cell_count paths from the worst to the best
-    foreach_in_collection path [get_timing_paths -slack_greater_than 0.0 -nworst $cell_count] {
-        set new_cells [get_cells_in_path $path]
-        foreach cell $new_cells {
-            set found 0
-            set cell_full_name [get_attribute $cell full_name]
-            foreach cell_in_list $priority_list {
-                set cell_in_list_full_name [get_attribute $cell_in_list full_name]
-                if {$cell_full_name eq $cell_in_list_full_name} {
-                    set found 1
-                    break
-                }
-            }
-            if {$found == 0} {
-                lappend priority_list $cell
-            }
-        }
-        # break if length of priority_list is equal to cell_count
-        if {[llength $priority_list] == $cell_count} {
-            break
-        }
-    }
+    foreach_in_collection cell $all_cells {
+        set this_cell_list [list $cell]
+        set this_wrst_path [get_timing_paths -through $cell -slack_greater_than 0.0 -nworst 1]
+        set this_slack [get_attribute $this_wrst_path slack]
+        lappend this_cell_list $this_slack
+        lappend priority_list $this_cell_list
 
-    # the priority list is ordered from the worst to the best
-    # reversing it to have the best first (we are going to swap the best ones first)
-    return [lreverse $priority_list]
+    }
+    set priority_list [lsort -index 1 -real $priority_list]
+    return $priority_list
 }
 
-proc swap_vt {cell vt} {
-    set library_name "CORE65LP${vt}VT"
+proc swap_vt {cell original_vt new_vt} {
+    set library_name "CORE65LP${new_vt}VT"
     set ref_name [get_attribute $cell ref_name]
-    regsub {_LL} $ref_name "_L${vt}" new_ref_name
+    regsub "_L${original_vt}" $ref_name "_L${new_vt}" new_ref_name
     size_cell $cell "${library_name}/${new_ref_name}"
 
     return
 }
 
-proc binary_swap {priority_list original_vt new_vt} {
+proc binary_swap {original_vt new_vt} {
     # case 1 swap L to H then L to S
     # case 2 swap L to S then S to H
+    # if the number of swaps in the current cycle is 0, return
+    set priority_list [create_priority_list]
+    set percentage 0.5
+    set vt_cells [get_cells -quiet -filter "lib_cell.threshold_voltage_group == $original_vt\VT"]
+    set num_cells [sizeof_collection $vt_cells]
+    set num_swaps [expr {int($num_cells * $percentage)}]
+    while {$num_swaps > 0} {
+        set success 0
+        set swapped_cells [list]
+        ##########################################################################
+        #                          LOGIC FOR SWAPPING                            #
+        ##########################################################################
+        # swap $num_swaps cells according to priority list
+        # update swapped_cells list
 
-    
-    # call this to update the timing of the combinational circuit (recalculate slack foreach cell)
-    # update_timing -full
+        # call this to update the timing of the combinational circuit (recalculate slack foreach cell)
+        update_timing -full
+        # get critical path with slack lesser than 0
+        # if no such path exists, set success to 1
 
-    # revert operations if negative slack
+
+        if {$success} {
+            set percentage [expr {$percentage + ($percentage / 2)}]
+            # update priority_list
+            set priority_list [create_priority_list]
+        }
+        else {
+            # revert operations using swapped_cells list
+            set percentage [expr {$percentage - ($percentage / 2)}]
+            # i think there's no need to update timing again in this case since we're reverting the operations
+        }
+        # update next cycle's number of swaps
+        set num_swaps [expr {int($num_cells * $percentage)}]
+    }
+    return
 }
 
 
@@ -83,7 +70,7 @@ proc multiVth {} {
         set C 100
         for {set i 0} {$i < $C} {incr i} {
             set cell [index_collection $lvt_cells $i]
-            swap_vt $cell "H"
+            swap_vt $cell "L" "H"
         }
     }
     return 1
